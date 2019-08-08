c5f3e0a2fa9e228e08891c6c74ace702bd03d8db
Nikolay Makhotkin <nikolay.makhotkin@gmail.com>
2019-08-08 19:45:51 +0400

    Print best threshold

--- evaluate.py
+++ evaluate.py
@@ -17,10 +17,15 @@ def get_args():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--mode', type=str, default='build', help='model mode: build')
-    parser.add_argument('--config_path', type=str, default='./configs/config_ms1m_100.yaml', help='config path, used when mode is build')
-    parser.add_argument('--model_path', type=str, default='/data/hhd/InsightFace-tensorflow/output/20190116-130753/checkpoints/ckpt-m-116000', help='model path')
-    parser.add_argument('--val_data', type=str, default='', help='val data, a dict with key as data name, value as data path')
-    parser.add_argument('--train_mode', type=int, default=0, help='whether set train phase to True when getting embds. zero means False, one means True')
+    parser.add_argument('--config_path', type=str, default='./configs/config_ms1m_100.yaml',
+                        help='config path, used when mode is build')
+    parser.add_argument('--model_path', type=str,
+                        default='/data/hhd/InsightFace-tensorflow/output/20190116-130753/checkpoints/ckpt-m-116000',
+                        help='model path')
+    parser.add_argument('--val_data', type=str, default='',
+                        help='val data, a dict with key as data name, value as data path')
+    parser.add_argument('--train_mode', type=int, default=0,
+                        help='whether set train phase to True when getting embds. zero means False, one means True')
     parser.add_argument('--target_far', type=float, default=1e-3, help='target far when calculate tar')
     parser.add_argument('--dataset_dir', help='path to dataset')
 
@@ -58,33 +63,46 @@ def load_bin(path, image_size):
 def evaluate(embeddings, actual_issame, far_target=1e-3, distance_metric=0, nrof_folds=10):
     thresholds = np.arange(0, 4, 0.01)
     if distance_metric == 1:
-        thresholdes = np.arange(0, 1, 0.0025)
+        thresholds = np.arange(0, 1, 0.0025)
     embeddings1 = embeddings[0::2]
     embeddings2 = embeddings[1::2]
-    tpr, fpr, accuracy = calculate_roc(thresholds, embeddings1, embeddings2, np.asarray(actual_issame), distance_metric=distance_metric, nrof_folds=nrof_folds)
-    tar, tar_std, far = calculate_tar(thresholds, embeddings1, embeddings2, np.asarray(actual_issame), far_target=far_target, distance_metric=distance_metric, nrof_folds=nrof_folds)
+    tpr, fpr, accuracy = calculate_roc(
+        thresholds, embeddings1, embeddings2, np.asarray(actual_issame),
+        distance_metric=distance_metric, nrof_folds=nrof_folds
+    )
+    tar, tar_std, far = calculate_tar(
+        thresholds, embeddings1, embeddings2, np.asarray(actual_issame),
+        far_target=far_target, distance_metric=distance_metric, nrof_folds=nrof_folds
+    )
     acc_mean = np.mean(accuracy)
     acc_std = np.std(accuracy)
     return tpr, fpr, acc_mean, acc_std, tar, tar_std, far
 
 
-def run_embds(sess, images, batch_size, image_size, train_mode, embds_ph, image_ph, train_ph_dropout, train_ph_bn):
+def run_embds(sess, images, batch_size, image_size, train_mode,
+              embds_ph, image_ph, train_ph_dropout, train_ph_bn):
     if train_mode >= 1:
         train = True
     else:
         train = False
-    batch_num = len(images)//batch_size
-    left = len(images)%batch_size
+    batch_num = len(images) // batch_size
+    left = len(images) % batch_size
     embds = []
     for i in range(batch_num):
-        image_batch = images[i*batch_size: (i+1)*batch_size]
-        cur_embd = sess.run(embds_ph, feed_dict={image_ph: image_batch, train_ph_dropout: train, train_ph_bn: train})
+        image_batch = images[i * batch_size: (i + 1) * batch_size]
+        cur_embd = sess.run(
+            embds_ph,
+            feed_dict={image_ph: image_batch, train_ph_dropout: train, train_ph_bn: train}
+        )
         embds += list(cur_embd)
         print('%d/%d' % (i, batch_num), end='\r')
     if left > 0:
         image_batch = np.zeros([batch_size, image_size, image_size, 3])
         image_batch[:left, :, :, :] = images[-left:]
-        cur_embd = sess.run(embds_ph, feed_dict={image_ph: image_batch, train_ph_dropout: train, train_ph_bn: train})
+        cur_embd = sess.run(
+            embds_ph,
+            feed_dict={image_ph: image_batch, train_ph_dropout: train, train_ph_bn: train}
+        )
         embds += list(cur_embd)[:left]
     print()
     print('done!')
@@ -134,13 +152,13 @@ def load_dataset(dataset_dir):
     return imgs, imgs_f, issame
 
 
-
 if __name__ == '__main__':
     args = get_args()
     if args.mode == 'build':
         print('building...')
         config = yaml.load(open(args.config_path))
-        images = tf.placeholder(dtype=tf.float32, shape=[None, config['image_size'], config['image_size'], 3], name='input_image')
+        images = tf.placeholder(dtype=tf.float32, shape=[None, config['image_size'], config['image_size'], 3],
+                                name='input_image')
         train_phase_dropout = tf.placeholder(dtype=tf.bool, shape=None, name='train_phase')
         train_phase_bn = tf.placeholder(dtype=tf.bool, shape=None, name='train_phase_last')
         embds, _ = get_embd(images, train_phase_dropout, train_phase_bn, config)
@@ -173,8 +191,12 @@ if __name__ == '__main__':
                 embds_arr = embds_arr / np.linalg.norm(embds_arr, axis=1, keepdims=True) + embds_f_arr / np.linalg.norm(
                     embds_f_arr, axis=1, keepdims=True)
                 print('done!')
-                tpr, fpr, acc_mean, acc_std, tar, tar_std, far = evaluate(embds_arr, issame, far_target=args.target_far,
-                                                                          distance_metric=0)
+                tpr, fpr, acc_mean, acc_std, tar, tar_std, far = evaluate(
+                    embds_arr,
+                    issame,
+                    far_target=args.target_far,
+                    distance_metric=0
+                )
                 print('eval: acc--%1.5f+-%1.5f, tar--%1.5f+-%1.5f@far=%1.5f' % (
                     acc_mean, acc_std, tar, tar_std, far
                 ))
@@ -188,12 +210,27 @@ if __name__ == '__main__':
                 for k, v in val_data.items():
                     imgs, imgs_f, issame = load_bin(v, config['image_size'])
                     print('forward running...')
-                    embds_arr = run_embds(sess, imgs, batch_size, config['image_size'], args.train_mode, embds, images, train_phase_dropout, train_phase_bn)
-                    embds_f_arr = run_embds(sess, imgs_f, batch_size, config['image_size'], args.train_mode, embds, images, train_phase_dropout, train_phase_bn)
-                    embds_arr = embds_arr/np.linalg.norm(embds_arr, axis=1, keepdims=True)+embds_f_arr/np.linalg.norm(embds_f_arr, axis=1, keepdims=True)
+                    embds_arr = run_embds(
+                        sess, imgs, batch_size, config['image_size'],
+                        args.train_mode, embds, images, train_phase_dropout, train_phase_bn
+                    )
+                    embds_f_arr = run_embds(
+                        sess, imgs_f, batch_size, config['image_size'],
+                        args.train_mode, embds, images, train_phase_dropout, train_phase_bn
+                    )
+                    embds_arr = (
+                        embds_arr / np.linalg.norm(embds_arr, axis=1, keepdims=True)
+                        + embds_f_arr / np.linalg.norm(embds_f_arr, axis=1, keepdims=True)
+                    )
                     print('done!')
-                    tpr, fpr, acc_mean, acc_std, tar, tar_std, far = evaluate(embds_arr, issame, far_target=args.target_far, distance_metric=1)
-                    print('eval on %s: acc--%1.5f+-%1.5f, tar--%1.5f+-%1.5f@far=%1.5f' % (k, acc_mean, acc_std, tar, tar_std, far))
+                    tpr, fpr, acc_mean, acc_std, tar, tar_std, far = evaluate(
+                        embds_arr,
+                        issame,
+                        far_target=args.target_far,
+                        distance_metric=1
+                    )
+                    print('eval on %s: acc--%1.5f+-%1.5f, tar--%1.5f+-%1.5f@far=%1.5f' % (
+                    k, acc_mean, acc_std, tar, tar_std, far))
                 print('done!')
     else:
         raise ValueError("Invalid value for --mode.")
